4e3747861bffbee15ddeac481d047ee1ef23b6f2
Tomi Toivio <tomi.toivio@ukuli.fi>
2020-08-14 13:42:58 +0300

    Error: AMQJS0010E WebSocket is not supported by this browser.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const userName = config.get('mqtt.userName');
 const password = config.get('mqtt.password');
 
 const Paho = require('paho-mqtt');
-const client = new Paho.MQTT.Client(location.hostname, Number(location.port), clientId);
+const client = new Paho.Client(hostname, Number(port), clientId);
  
 client.onConnectionLost = onConnectionLost;
 client.onMessageArrived = onMessageArrived;
@@ -19,14 +19,12 @@ var radioactivity = 3.6;
 function onConnect() {
     console.log("onConnect");
     client.subscribe("ukuli/" + client_id + "/radioactivity/");
+    client.subscribe("ukuli/" + client_id + "/reactorstatus/");
+    client.subscribe("ukuli/" + client_id + "/reactoroperator/");
     while(true) {
-        radioactivity = (Math.random() * 1000);
-        message = new Paho.MQTT.Message(radioactivity);
-        message.destinationName = "ukuli/" + client_id + "/radioactivity/";
-        client.send(message);
+        setTimeout(spamMessage(), 5000);
     }
 }
-
  
 function onConnectionLost(responseObject) {
     if (responseObject.errorCode !== 0) {
@@ -36,4 +34,47 @@ function onConnectionLost(responseObject) {
  
 function onMessageArrived(message) {
         console.log("onMessageArrived:"+message.payloadString);
+}
+
+function spamMessage() {
+    var randomNumber = Math.round((Math.random() * 10));
+    switch(randomNumber) {
+    // Reactor status
+    case 0:
+        var reactorstatus = Math.round((Math.random() * 2));
+        switch(reactorstatus) {
+            case 0:
+                reactorstatus = "Xenon poisoning";
+            break;
+            case 1:
+                reactorstatus = "Stable";
+            break;
+            default:
+                reactorstatus = "Meltdown";
+        }        
+    break;
+    // Reactoroperator
+    case 1:
+        var reactoroperator = Math.round((Math.random() * 2));
+        switch(reactoroperator) {
+            case 0:
+                reactoroperator = "Akimov";
+            break;
+            case 1:
+                reactoroperator = "Toptunov";
+            break;
+            default:
+                reactoroperator = "Dyatlov";
+        }
+        message = new Paho.MQTT.Message(reactoroperator);
+        message.destinationName = "ukuli/" + client_id + "/reactoroperator/";
+        client.send(message);
+    break;
+    // Radioactivity level
+    default:
+        radioactivity = (Math.random() * 1000);
+        message = new Paho.MQTT.Message(radioactivity);
+        message.destinationName = "ukuli/" + client_id + "/radioactivity/";
+        client.send(message);
+    }
 }
